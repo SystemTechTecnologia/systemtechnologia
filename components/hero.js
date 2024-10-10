45174dbd12f516188281c2908e31c4ba740c110a
Anderson Leite <68247603+AndersonLML@users.noreply.github.com>
2024-10-09 21:11:24 -0300

    update hero.js

--- components/hero.js
+++ components/hero.js
@@ -13,6 +13,11 @@ const Hero = () => {
             Faça doação a uma entidade social - clique aqui
             </a>
             </p>
+            <p className="py-5 text-xl leading-normal text-green-500 lg:text-xl xl:text-2xl dark:text-gray-300 font-bold">
+            <a href='https://casasandreluiz.org.br/#doacoes'>
+            Faça doação a uma entidade social - clique aqui
+            </a>
+            </p>
             <h1 className="text-4xl font-bold leading-snug tracking-tight text-gray-800 lg:text-4xl lg:leading-tight xl:text-6xl xl:leading-tight dark:text-white ">
               assistencia tecnica
             </h1>
